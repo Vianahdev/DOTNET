b2021fbf628ba9143060e758c8400f0fe6e5f7f8
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2024-01-26 15:52:24 -0300

    Feat: Estrutura condicional IF/ELSE

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -4,21 +4,16 @@ namespace CursoCSharp{
     class Program{
         static void Main(String [] args){
            int v_valor = 0;
-
-           switch (v_valor){
-
-            case 1: Console.WriteLine($"Olá Mundo o valor é: {v_valor}");
-            break;
-
-            case 2: Console.WriteLine($"Olá o valor é: {v_valor}");
-            break;
-
-            case 3: Console.WriteLine($"Olá Mundo, é: {v_valor}");
-            break;
-
-            default: Console.WriteLine("Olá Mundo o valor é: Null");
-            break;
-           }
+        
+            if(v_valor == 1){
+                Console.WriteLine($"Passou aqui e o valor é: {v_valor}");
+            }else if(v_valor == 2){
+                Console.WriteLine($"Passou aqui e o valor é: {v_valor}");
+            }else if(v_valor == 3){
+                Console.WriteLine($"Passou aqui e o valor é: {v_valor}");            
+            }else {
+                Console.WriteLine("Passou aqui e não possui valor");
+            }
         }
     }
 }
