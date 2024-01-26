02664a59e6b114d07ac9c15924034b5839dc5606
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2024-01-26 15:31:28 -0300

    Feat: Estrutura condicional Switch

---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CursoCSharp{
+    class Program{
+        static void Main(String [] args){
+           int v_valor = 0;
+
+           switch (v_valor){
+
+            case 1: Console.WriteLine($"Olá Mundo o valor é: {v_valor}");
+            break;
+
+            case 2: Console.WriteLine($"Olá o valor é: {v_valor}");
+            break;
+
+            case 3: Console.WriteLine($"Olá Mundo, é: {v_valor}");
+            break;
+
+            default: Console.WriteLine("Olá Mundo o valor é: Null");
+            break;
+           }
+        }
+    }
+}
+ 
