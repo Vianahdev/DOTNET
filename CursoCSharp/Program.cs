91b443c0672bedd4aab8fc4b5fe5ce643181f52c
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2024-02-03 19:37:56 -0300

    Feat: Utilizando método construtor

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -3,30 +3,37 @@
 namespace CursoCSharp{
     class Program{
         static void Main(String [] args){
-            var product = new Product();
+            //Simplificando a instancia do product tipo Objeto
+            var product = new Product{
 
+                Product_id = 1,
+                Product_name = "Teclado Gamer",
+                Product_price = 150.45,
 
-            product.Id = 1;
-            product.Name_Product = "Teclado Gamer";
-            product.Price_Product = 150.45f;
+            };
             
-            
-            Console.WriteLine($"Id do Produto: {product.Id}");
-            Console.WriteLine($"Nome do Produto: {product.Name_Product}");
-            Console.WriteLine($"Preço do Produto: {product.Price_Product}");
-            Console.WriteLine($"Preço do Produto: {product.PriceInDolar(4.97f)}");
+            Console.WriteLine($"Id do Produto: {product.Product_id}");
+            Console.WriteLine($"Nome do Produto: {product.Product_name}");
+            Console.WriteLine($"Preço do Produto: {product.Product_price}");
+            Console.WriteLine($"Preço do Produto: {product.PriceInDolar(4.97)}");
             
         }
 
     }
 
     struct Product{
-        public int Id;
-        public string Name_Product;
-        public float Price_Product;
+        public Product(int Product_Id, string Product_Name, double Product_Price){
+            Product_Id = Product_id;
+            Product_Name = Product_name;
+            Product_Price = Product_price;
+        }
 
-        public float PriceInDolar (float dolar){
-            return Price_Product * dolar;
+        public int Product_id {get; set;}
+        public string Product_name {get; set;}
+        public double Product_price {get; set;}
+
+        public double PriceInDolar (double dolar){
+            return Product_price * dolar;
         }
 
     }
