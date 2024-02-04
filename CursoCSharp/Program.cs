dbcc2875d3844e6ef51ab054e0a74320c1a94031
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2024-02-03 21:29:43 -0300

    Feat: Enumeradores
    Utilizando Enumeradores para simplificar  o código.

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -3,39 +3,32 @@
 namespace CursoCSharp{
     class Program{
         static void Main(String [] args){
-            //Simplificando a instancia do product tipo Objeto
-            var product = new Product{
-
-                Product_id = 1,
-                Product_name = "Teclado Gamer",
-                Product_price = 150.45,
-
+            
+            var cliente = new Cliente{
+                Cliente_name = "Matheus",
+                Cliente_idade = 24,
+                EstadoCivil = EEstadoCivil.Solteiro
             };
-            
-            Console.WriteLine($"Id do Produto: {product.Product_id}");
-            Console.WriteLine($"Nome do Produto: {product.Product_name}");
-            Console.WriteLine($"Preço do Produto: {product.Product_price}");
-            Console.WriteLine($"Preço do Produto: {product.PriceInDolar(4.97)}");
-            
+
+        
+            Console.WriteLine($"Nome Cliente: {cliente.Cliente_name}");
+            Console.WriteLine($"Idade Cliente: {cliente.Cliente_idade}");
+            Console.WriteLine($"Estado Civil: {cliente.EstadoCivil}");
         }
 
     }
 
-    struct Product{
-        public Product(int Product_Id, string Product_Name, double Product_Price){
-            Product_Id = Product_id;
-            Product_Name = Product_name;
-            Product_Price = Product_price;
-        }
+    struct Cliente{
+        public string Cliente_name;
+        public int Cliente_idade;
+        public EEstadoCivil EstadoCivil;
+        
+    }
 
-        public int Product_id {get; set;}
-        public string Product_name {get; set;}
-        public double Product_price {get; set;}
-
-        public double PriceInDolar (double dolar){
-            return Product_price * dolar;
-        }
-
+    enum EEstadoCivil{
+        Solteiro = 1,
+        Casado = 2,
+        Divorciado = 3,
     }
 }
  
--- CursoCSharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ CursoCSharp/obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b7d91ed1992ef782351b0249bd531cd136cb44c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b443c0672bedd4aab8fc4b5fe5ce643181f52c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
